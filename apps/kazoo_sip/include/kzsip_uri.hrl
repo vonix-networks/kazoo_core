22ab7e5ba321539b95a969ea22fe7aa09aec6011
markxu <markxfl@gmail.com>
2025-07-25 12:29:00 -0400

    Revert "remove nklib dependency to resolve duplicate localtime modules"
    This reverts commit 71f857691e333f9d95904b572992851a0cb0b6a7.

--- apps/kazoo_sip/include/kzsip_uri.hrl
+++ apps/kazoo_sip/include/kzsip_uri.hrl
@@ -1,19 +1,6 @@
 -ifndef(KZSIP_URI_HRL).
 
--record(uri, {
-    scheme :: nklib:scheme(),
-    user = <<>> :: binary(),
-    pass = <<>> :: binary(),
-    domain = <<"invalid.invalid">> :: binary(),
-    % 0 means "no port in message"
-    port = 0 :: inet:port_number(),
-    path = <<>> :: binary(),
-    opts = [] :: list(),
-    headers = [] :: list(),
-    ext_opts = [] :: list(),
-    ext_headers = [] :: list(),
-    disp = <<>> :: binary()
-}).
+-include_lib("nklib/include/nklib.hrl").
 
 -define(KZSIP_URI_HRL, 'true').
 -endif.
